e173c25e84642ce5eea6b0a3cc959fac54769530
Ayoub El Younssi <r0949573@student.thomasmore.be>
2023-10-05 19:48:45 +0200

    Add project files.

---
+++ BlazorOpdracht/MovieSearchResponse.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualBasic;
+using System.ComponentModel;
+using System.Net.NetworkInformation;
+using System;
+
+namespace MovieSearchResponse 
+{
+    public class MovieSearchResponse
+    {
+        public string? Response { get; set; }
+        public int TotalResults { get; set; }
+        public List<Movie>? Search { get; set; }
+    }
+
+    public class Movie
+    {
+        public string? Title { get; set; }
+        public string? Year { get; set; }
+        public string? Poster { get; set; }
+    }
+}
+
+
+
+
+
---
+++ BlazorOpdracht/Program.cs
@@ -0,0 +1,14 @@
+using BlazorOpdracht;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using static System.Net.Mime.MediaTypeNames;
+using System.Text.Json;
+
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
